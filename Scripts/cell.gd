eb1e9cb4e6ec01d8b4b1bc10ba8d7bd8b54632a7
Mister_yay <gettingbetter.y@gmail.com>
2025-06-14 14:45:53 -0600

    Added basic interaction with cells
    added the basic cell of the game. This cell can be oppened or flagged

---
+++ Scripts/cell.gd
@@ -0,0 +1,41 @@
+extends Sprite2D
+
+var pressed : bool = false
+var flagged: bool = false
+
+func open_cell() -> void:
+	if flagged:
+		return
+	
+	print(self, " pressed")
+	var pressed_texutre = preload("res://Assets/Cell/pressed_cell.png")
+	texture = pressed_texutre
+	pressed = true
+	
+
+
+func flag_cell() -> void:
+	
+	if pressed:
+		return
+	if not flagged:
+		print(self, " Flagged")
+		var flagged_texture = preload("res://Assets/Cell/flagged_cell.png")
+		texture = flagged_texture
+		flagged = true
+		return
+		
+	var normal_texture = preload("res://Assets/Cell/normal_cell.png")
+	texture = normal_texture
+	flagged = false
+
+
+func _on_control_gui_input(event: InputEvent) -> void:
+	if pressed:
+		return
+		
+	
+	if event is InputEventMouseButton and event.is_released() and event.button_index == MOUSE_BUTTON_LEFT:
+		open_cell()
+	elif event is InputEventMouseButton and event.is_released() and event.button_index == MOUSE_BUTTON_RIGHT:
+		flag_cell()
