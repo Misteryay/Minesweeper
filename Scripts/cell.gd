d462e197fa9f9134f858069df78ccc6cee854b2f
Mister_yay <gettingbetter.y@gmail.com>
2025-06-17 17:35:07 -0600

    Grid generator functionality
    Added a function to get a cell's neighbours

--- Scripts/cell.gd
+++ Scripts/cell.gd
@@ -1,9 +1,12 @@
 extends Sprite2D
 
+signal empty_cell(cell)
+
 var pressed : bool = false
 var flagged: bool = false
 var has_mine: bool = false
-var mines_around: int
+var mines_around: int = 0
+var coordinates: Vector2 = Vector2.ZERO
 
 func open_cell() -> void:
 	if flagged:
@@ -12,12 +15,34 @@ func open_cell() -> void:
 	if has_mine:
 		var mine_texture = preload("res://Assets/Cell/mine.png")
 		texture = mine_texture
+		pressed = true
 		return
 	
-	print(self, " pressed")
-	var pressed_texutre = preload("res://Assets/Cell/pressed_cell.png")
-	texture = pressed_texutre
+	var pressed_texture : Texture 
+	match mines_around:
+		0:
+			pressed_texture = preload("res://Assets/Cell/pressed_cell.png")
+			emit_signal("empty_cell", self)
+		1:
+			pressed_texture = preload("res://Assets/Cell/1_mine.png")
+		2:
+			pressed_texture = preload("res://Assets/Cell/2_mines.png")
+		3:
+			pressed_texture = preload("res://Assets/Cell/3_mines.png")
+		4:
+			pressed_texture = preload("res://Assets/Cell/4_mines.png")
+		5:
+			pressed_texture = preload("res://Assets/Cell/5_mines.png")
+		6:
+			pressed_texture = preload("res://Assets/Cell/6_mines.png")
+		7:
+			pressed_texture = preload("res://Assets/Cell/7_mines.png")
+		8:
+			pressed_texture = preload("res://Assets/Cell/8_mines.png")
+	
+	texture = pressed_texture
 	pressed = true
+	
 
 func flag_cell() -> void:
 	
--- grid_generator.gd
+++ grid_generator.gd
@@ -1,40 +1,111 @@
 extends Node2D
 
-@export var cell: PackedScene
+@export var cell_scene: PackedScene
 @export var cuadratic_size: int = 3
 
 const CELL_WIDTH = 32
 const CELL_HEIGHT = 32
 
-
+var grid = []
+var total_mines : int = 0
+var cells_grid: Array
 
 func _ready() -> void:
-	position = (get_viewport_rect().size / 2) - ((Vector2(CELL_WIDTH, CELL_HEIGHT) * cuadratic_size) / 2)
-	var cells: Array = generate_grid()
+	center_grid()
+
+	grid.resize(cuadratic_size)
+	for y in range(cuadratic_size):
+		grid[y] = []
+		grid[y].resize(cuadratic_size)
+		
+	var cells: Array[Array] = generate_grid()
+	cells_grid = cells
 	add_random_bombs(cells)
+	get_grid_map(cells)
+	set_mines_around_cell(cells)
 	
+	#print_grid()
+	
+func center_grid() -> void:
+	position = (get_viewport_rect().size / 2) - ((Vector2(CELL_WIDTH, CELL_HEIGHT) * cuadratic_size) / 2)
 
-
-func generate_grid() -> Array:
+func generate_grid() -> Array[Array]:
 	var x_origin: int = 0
 	var y_origin: int = 0
-	var cells: Array = []
+	var cells: Array[Array]
 	
-	for y in cuadratic_size:
+	for y in range(cuadratic_size):
+		var cells_row: Array
 		x_origin = 0
-		for x in cuadratic_size:
-			var new_cell = cell.instantiate()
+		
+		for x in range(cuadratic_size):
+			var new_cell = cell_scene.instantiate()
+			new_cell.coordinates = Vector2(x, y)
 			add_child(new_cell)
 			new_cell.position.x = x_origin
 			new_cell.position.y = y_origin
 			x_origin += CELL_WIDTH
-			cells.append(new_cell)
-			
+			cells_row.append(new_cell)
+		
+		cells.append(cells_row)
 		y_origin += CELL_HEIGHT
 	return cells
 
-func add_random_bombs(cells: Array) -> void:
-	for cell in cells:
-		var random_number = randf()
-		if random_number < 0.20:
-			cell.has_mine = true
+func add_random_bombs(cells: Array[Array]) -> void:
+	for row in range(cuadratic_size):
+		for column in range(cuadratic_size):
+			var rand_number = randf()
+			if rand_number < 0.20:
+				var cell = cells[column][row]
+				cell.has_mine = true
+				total_mines *= 1
+
+func get_grid_map(cells: Array[Array]) -> void:
+	for row in range(cuadratic_size):
+		for column in range(cuadratic_size):
+			var current_cell = cells[column][row]
+			if current_cell.has_mine:
+				grid[column][row] = 1
+			else:
+				grid[column][row] = 0
+
+func set_mines_around_cell(cells: Array[Array]) -> void:
+	for row in range(cuadratic_size):
+		for column in range(cuadratic_size):
+			var current_cell = cells[column][row]
+			if current_cell.has_mine:
+				continue
+			
+			var neighbour_cells = get_cell_neighbours(current_cell, cells)
+			for neighbour in neighbour_cells:
+				if neighbour.has_mine:
+					current_cell.mines_around += 1
+
+					
+func get_cell_neighbours(cell, cells) -> Array:
+	# Returns an array of the neighbors relative to that cell's position
+	var current_cell_x = cell.coordinates.x
+	var current_cell_y = cell.coordinates.y
+	
+	var neighbour_cells: Array = []
+	for y in range(-1, 2):
+		var neighbor_y = current_cell_y + y
+		if neighbor_y < 0 or neighbor_y >= cuadratic_size:
+			continue
+		for x in range(-1, 2):
+			var neighbor_x = current_cell_x + x
+			if neighbor_x < 0 or neighbor_x >= cuadratic_size:
+				continue
+					
+			var neighbor_cell = cells[neighbor_y][neighbor_x]
+			neighbour_cells.append(neighbor_cell)
+	return neighbour_cells
+
+func print_grid() -> void:
+	# Debug porpuses only
+	# Prints the grid Array on console
+	for y in range(cuadratic_size):
+		var line := ""
+		for x in range(cuadratic_size):
+			line += "%d " % grid[y][x]
+		print(line.strip_edges())
